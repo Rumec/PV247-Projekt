8bc9ecaff038b490e2dbd2b2bdd9cb49b313002b
Rumec <drumian@seznam.cz>
2022-11-26 14:06:16 +0100

    Add: theme

---
+++ pv247-project-weather-app/src/utils/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: { main: '#f2d45c' },
+		mode: 'dark'
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: {
+				// Css rule that makes sure app is always 100% height of window
+				'body, #root': {
+					display: 'flex',
+					flexDirection: 'column',
+					minHeight: '100vh',
+					minWidth: '100vw'
+				}
+			}
+		}
+	}
+});
+
+export default theme;
